c2a7dc06d3c734e2a68916c14c1022be30656504
eleanormng <eleanormng@gmail.com>
2023-05-11 12:49:09 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,12 +1,29 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+# Used to remove past graded submissions, create clean slate
 rm -rf student-submission
 rm -rf grading-area
 
+# Make directory grading-area
 mkdir grading-area
 
+# Clone the student repository
 git clone $1 student-submission
 echo 'Finished cloning'
+
+# Change from List-Examples-Grader directory to student-submission directory
+cd student-submission
+
+# Check if the ListExamples.java file exists
+if [ -e ListExamples.java ]
+then
+    echo 'File exists'
+else
+    echo 'File not found'
+fi
+
+# Copy files from student-submission directory to the grading-area directory
+cp -r student-submission grading-area
 
 
 # Draw a picture/take notes on the directory structure that's set up after
